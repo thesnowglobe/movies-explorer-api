22d2655f2c5b22dcc2558316f7012b9f19dd752e
Alena <102154806+thesnowglobe@users.noreply.github.com>
2024-10-04 22:35:15 +0200

    console log err

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const { serverError } = require('../constants/errorsMessages');
 
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-
+  console.error(message);
   res.status(statusCode).send({
     message: statusCode === 500
       ? serverError
